1e76b69ff6787250ffb33f81037ec2a42004cc40
Abdul8971 <145859804+Abdul8971@users.noreply.github.com>
2024-11-18 14:37:39 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,27 +2,12 @@ import React, { useState, useEffect } from "react";
 import TransactionsTable from "./components/TransactionsTable/TransactionsTable";
 import "./App.css";
 
+if (window.innerWidth < 400) {
+  alert("Please open in desktop mode");
+} 
+
 const App = () => {
-  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 1000);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsDesktop(window.innerWidth > 1000);
-    };
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  if (!isDesktop) {
-    return (
-      <div className="mobile-warning">
-        <h1>Please open in desktop mode</h1>
-      </div>
-    );
-  }
-
+  
   return (
     <>
       <TransactionsTable />
